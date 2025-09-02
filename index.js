9b2940da4ab00a06a6afdc326d3d8e20d7b0aee2
miguelenne <58816225+miguelenne@users.noreply.github.com>
2025-09-02 20:04:47 -0300

    Corrigido erro que impedia o modo escuro e modo de leitura de funcionar adequadamente na página de lista dos papas

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ function modoLeitura(){
         if (nome !== null){ nome.setAttribute("style", "color: black;"); }
         if (header !== null){ header.setAttribute("style", "color: black;"); }
         if (textoIndex !== null){ textoIndex.setAttribute("style", "color: black"); }
+        if (barraPapa !== null){
+            for(i=0;i<barraPapa.length;i++){ barraPapa[i].setAttribute("style", "color: black"); }
+        }
+       
 
     }
 
     else {
-        body.setAttribute("style", "background-color: white;");
+        body.setAttribute("style", "background-color: white"); 
         if (textoDocumento !== null) { textoDocumento.setAttribute("style", "color: black"); }
         if (tipo !== null) { tipo.setAttribute("style", "color: black"); }
         if (descricao !== null) { descricao.setAttribute("style", "color: black"); }
@@ -20,6 +24,9 @@ function modoLeitura(){
         if (nome !== null){ nome.setAttribute("style", "color: black"); }
         if (header !== null){ header.setAttribute("style", "color: black"); }
         if (textoIndex !== null){ textoIndex.setAttribute("style", "color: black"); }
+        if (barraPapa !== null){
+            for(i=0;i<barraPapa.length;i++){ barraPapa[i].setAttribute("style", "color: black"); }
+        }
     }
     
   
@@ -27,7 +34,7 @@ function modoLeitura(){
 
 function modoEscuro(){
     if (this.checked) {
-        body.setAttribute("style", "background-color: black;");
+        body.setAttribute("style", "background-color: black");
         if (textoDocumento !== null) { textoDocumento.setAttribute("style", "color: #e8e6e3"); }
         if (tipo !== null) { tipo.setAttribute("style", "color: #e8e6e3"); }
         if (descricao !== null) { descricao.setAttribute("style", "color: #e8e6e3"); }
@@ -35,10 +42,13 @@ function modoEscuro(){
         if (nome !== null){ nome.setAttribute("style", "color: #e8e6e3"); }
         if (header !== null){ header.setAttribute("style", "color: #e8e6e3"); }
         if (textoIndex !== null){ textoIndex.setAttribute("style", "color: #e8e6e3"); }
+        if (barraPapa !== null){
+            for(i=0;i<barraPapa.length;i++){ barraPapa[i].setAttribute("style", "color: white"); }
+        }
     } 
   
     else {
-        body.setAttribute("style", "background-color: white;");
+        body.setAttribute("style", "background-color: white");
         if (textoDocumento !== null) { textoDocumento.setAttribute("style", "color: black"); }
         if (tipo !== null) { tipo.setAttribute("style", "color: black"); }
         if (descricao !== null) { descricao.setAttribute("style", "color: black"); }
@@ -46,13 +56,17 @@ function modoEscuro(){
         if (nome !== null){ nome.setAttribute("style", "color: black"); }
         if (header !== null){ header.setAttribute("style", "color: black"); }
         if (textoIndex !== null){ textoIndex.setAttribute("style", "color: black"); }
+        if (barraPapa !== null){
+            for(i=0;i<barraPapa.length;i++){ barraPapa[i].setAttribute("style", "color: black"); }
+        }
     }
 }
 
 
-const checkbox1 = document.querySelector("#switch-basic-1");
-const checkbox2 = document.querySelector("#switch-basic-2");
+const darkModeSwitch = document.querySelector("#switch-basic-1");
+const readModeSwitch = document.querySelector("#switch-basic-2");
 const switchWrapper = document.querySelector(".switch-wrapper");
+const barraPapa = document.querySelectorAll(".barraPapa");
 const body = document.querySelector("body");
 const textoDocumento = document.querySelector(".textoDocumento");
 const nome = document.querySelector(".nomePapa");
@@ -60,5 +74,5 @@ const tipo = document.querySelector(".tipoDocumento");
 const descricao = document.querySelector(".descricaoDocumento");
 const header = document.querySelector(".header");
 const textoIndex = document.querySelector(".documentosRecentes");
-checkbox2.addEventListener("change", modoLeitura)
-checkbox1.addEventListener('change', modoEscuro)
+readModeSwitch.addEventListener("change", modoLeitura)
+darkModeSwitch.addEventListener('change', modoEscuro)
